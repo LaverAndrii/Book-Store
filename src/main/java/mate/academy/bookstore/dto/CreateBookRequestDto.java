53d939d48800803bfd98de42371195e2e21f1403
Andrii Laver <laver.andriy@gmail.com>
2024-03-13 13:37:15 +0200

    swapped NotNull with NotBalnk annotaions

--- src/main/java/mate/academy/bookstore/dto/CreateBookRequestDto.java
+++ src/main/java/mate/academy/bookstore/dto/CreateBookRequestDto.java
@@ -1,17 +1,18 @@
 package mate.academy.bookstore.dto;
 
 import jakarta.validation.constraints.Min;
+import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
 import java.math.BigDecimal;
 import lombok.Data;
 
 @Data
 public class CreateBookRequestDto {
-    @NotNull
+    @NotBlank(message = "may not be blank")
     private String title;
-    @NotNull
+    @NotBlank(message = "may not be blank")
     private String author;
-    @NotNull
+    @NotBlank(message = "may not be blank")
     private String isbn;
     @NotNull
     @Min(0)
